5d7779c11a28d0ba967b77401c04a4259e42291c
unknown <cruzmaralis172813@email.com>
2026-05-24 09:53:35 -0400

    Modificación para credenciales de usuario

--- users/serializers.py
+++ users/serializers.py
@@ -2,17 +2,26 @@ from rest_framework import serializers
 
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+
         model = User
-        fields = [
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            'telefono',
-            'direccion',
-            'ci',
-            'rol',
-        ]
+        fields = '__all__'
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+
+        password = validated_data.pop('password')
+
+        user = User(**validated_data)
+
+        user.set_password(password)
+
+        user.save()
+
+        return user
