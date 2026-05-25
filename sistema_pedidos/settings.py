088dd68ad080215afc276815bad771686ce04120
unknown <cruzmaralis172813@email.com>
2026-05-25 17:24:55 -0400

    Actualización del de settings

--- sistema_pedidos/settings.py
+++ sistema_pedidos/settings.py
@@ -1,27 +1,32 @@
+from decouple import config
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 import dj_database_url
 
-load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ---------------- MEDIA ----------------
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ---------------- AUTH ----------------
 AUTH_USER_MODEL = 'users.User'
 
 # ---------------- SECURITY ----------------
-SECRET_KEY = 'django-insecure-pmd$48znr6ux*%i80a3u5_4s35ta2_fwh+q4c#6$ke=z4!n#cj'
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    "sistema-pedidos-rqhc.onrender.com",
-    "front-end-sistema-pedidos-quxl-huo268ope.vercel.app",
+    '.onrender.com',
+    '.vercel.app',
+    'localhost',
+    '127.0.0.1'
 ]
 
 # ---------------- APPS ----------------
@@ -43,8 +48,12 @@ INSTALLED_APPS = [
 
 # ---------------- MIDDLEWARE (ORDEN IMPORTANTE) ----------------
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # 👈 DEBE IR ARRIBA
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,12 +64,10 @@ MIDDLEWARE = [
 
 # ---------------- CORS CONFIG (CORREGIDO) ----------------
 
-CORS_ALLOW_ALL_ORIGINS = True  # 🔥 CLAVE PARA TU CASO
 
 CORS_ALLOWED_ORIGINS = [
     "https://front-end-sistema-pedidos-quxl-huo268ope.vercel.app",
 ]
-
 CSRF_TRUSTED_ORIGINS = [
     "https://front-end-sistema-pedidos-quxl-huo268ope.vercel.app",
 ]
@@ -73,7 +80,7 @@ WSGI_APPLICATION = 'sistema_pedidos.wsgi.application'
 # ---------------- DATABASE ----------------
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=config("DATABASE_URL")
     )
 }
 
@@ -89,8 +96,40 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
 
-# ---------------- STATIC ----------------
-STATIC_URL = 'static/'
-
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+]
 # ---------------- DEFAULT ----------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+
+                'django.template.context_processors.request',
+
+                'django.contrib.auth.context_processors.auth',
+
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+LANGUAGE_CODE = 'es'
+
+TIME_ZONE = 'America/La_Paz'
+
+USE_I18N = True
+
+USE_TZ = True
