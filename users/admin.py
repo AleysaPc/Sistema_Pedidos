83fa9adf654a4cc3ada30b3686b77d86b86ef633
unknown <cruzmaralis172813@email.com>
2026-05-25 21:22:42 -0400

    Mod. Django-Admin user

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,51 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+
+    model = User
+
+    # Campos que se muestran en la tabla
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'rol',
+        'is_staff',
+        'is_active',
+    )
+
+    # Campos editables al entrar al usuario
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Información adicional',
+            {
+                'fields': (
+                    'telefono',
+                    'direccion',
+                    'ci',
+                    'rol',
+                )
+            },
+        ),
+    )
+
+    # Campos al crear usuario
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            'Información adicional',
+            {
+                'fields': (
+                    'email',
+                    'telefono',
+                    'direccion',
+                    'ci',
+                    'rol',
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
